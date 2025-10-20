2c9231d0a75ea9cdb50024ce10f3a957ff32a923
Emre Erturk <e.erturk@enbw.com>
2025-10-20 21:47:28 +0300

    Some days added

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,17 @@
-bmi = 84 / 1.65 ** 2
+# # # bmi = 84 / 1.65 ** 2
+# # # print(round(bmi))
+# # # print(round(3.6))
+# # # print(round(bmi, 3))
+# #
+# score = 2
+# score /= 3
+# # print(score)
+#
+# print("Yor score is " + (str(score)))
+
+score = 0
+height = 1.8
+is_winning = True
+
+# print("Your score is = " + str(score), "Your height is =" + str(height), "Your winning is = "  + str(is_winning))
+print(f"Your score is: {score}", f"Your height is: {height} You are winning value is: {is_winning}") # practicee
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,12 @@
-len(12345)
+from xmlrpc.client import Boolean, boolean
+
+# len({12345})
+# #
+# # print(type("hello"))
+# # print(type(123))
+# # print(type(123.11231231233))
+# # print(type(bool(False)))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+
